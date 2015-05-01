a188173df04ecbbe56e1f8e77c01761de3224015
Jose Miguel Navarro <jmnavarro@cloudroom.co>
2015-05-01 09:56:52 +0200

    added code

---
+++ RockPaperScissors/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using RockPaperScissors.Resources;
+using System.Windows.Media.Imaging;
+
+namespace RockPaperScissors
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        RPSGame rps = new RPSGame("piedra", "papel", "tijera");
+
+        int wins = 0, losses = 0, draws = 0;
+
+        string usuario = "";
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+
+        private void jugar()
+        {
+            ButtonsPanel.Visibility = System.Windows.Visibility.Collapsed;
+
+            int resultado = rps.Next(usuario);
+
+            switch (resultado)
+            {
+                case 1:
+                    wins++;
+                    textVictoriasValor.Text = wins.ToString();
+                    textResultado.Text = AppResources.ResourceManager.GetString("Victoria", AppResources.Culture);
+                    break;
+                case 0:
+                    draws++;
+                    textEmpatesValor.Text = draws.ToString();
+                    textResultado.Text = AppResources.ResourceManager.GetString("Empate", AppResources.Culture);
+                    break;
+                case -1:
+                    losses++;
+                    textDerrotasValor.Text = losses.ToString();
+                    textResultado.Text = AppResources.ResourceManager.GetString("Derrota", AppResources.Culture);
+                    break;
+            }
+            actualizarImagenes();
+            botonOtraPartida.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void actualizarImagenes()
+        {
+            string oponente = rps.LastAIWeapon;
+            imagenRival.Source = new BitmapImage(new Uri("/Imagenes/" + oponente + ".png", UriKind.Relative));
+            imagenUsuario.Source = new BitmapImage(new Uri("/Imagenes/" + usuario + ".png", UriKind.Relative));
+        }
+        private void Piedra_Click(object sender, RoutedEventArgs e)
+        {
+            usuario = "piedra";
+            jugar();
+        }
+
+        private void Papel_Click(object sender, RoutedEventArgs e)
+        {
+            usuario = "papel";
+            jugar();
+        }
+
+        private void Tijera_Click(object sender, RoutedEventArgs e)
+        {
+            usuario = "tijera";
+            jugar();
+        }
+
+        private void botonOtraPartida_Click(object sender, RoutedEventArgs e)
+        {
+            botonOtraPartida.Visibility = System.Windows.Visibility.Collapsed;
+            ButtonsPanel.Visibility = System.Windows.Visibility.Visible;
+
+            textResultado.Text = "";
+
+            imagenRival.Source = new BitmapImage(new Uri("/Imagenes/oponente.png", UriKind.Relative));
+            imagenUsuario.Source = new BitmapImage(new Uri("/Imagenes/usuario.png", UriKind.Relative));
+        }
+        
+        private void reiniciar_Click(object sender, EventArgs e)
+        {
+            wins=0;
+            textVictoriasValor.Text = wins.ToString();
+            draws=0;
+            textEmpatesValor.Text = draws.ToString();
+            losses=0;
+            textDerrotasValor.Text = losses.ToString();
+        }
+
+    }
+}
---
+++ RockPaperScissors/RPSGame.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class RPSGame
+    {
+        public RPSGame(params string[] weapons)
+        {
+            Weapons = weapons;
+
+            // Creates a new AI opponent, and gives it the list of weapons.
+            _rpsAI = new RPSAI(weapons);
+        }
+
+        // Play next turn.
+        public int Next(string weapon)
+        {
+            string aiWeapon = _rpsAI.NextMove(); // Gets the AI opponent's next move.
+            LastAIWeapon = aiWeapon; // Saves the AI opponent's move in a property so the player can see it.
+
+            _rpsAI.AddPlayerMove(weapon); // Let the AI know which weapon the player chose, for future predictions.
+            return GetWinner(Weapons, weapon, aiWeapon); // Returns -1 if AI win, 0 if draw, and 1 if player win.
+        }
+
+        // Returns matchup winner.
+        public static int GetWinner(string[] weapons, string weapon1, string weapon2)
+        {
+            if (weapon1 == weapon2)
+                return 0; // If weapons are the same, return 0 for draw.
+
+            if (GetVictories(weapons, weapon1).Contains(weapon2))
+                return 1; // Returns 1 for weapon1 win.
+            else if (GetVictories(weapons, weapon2).Contains(weapon1))
+                return -1; // Returns -1 for weapon2 win.
+
+            throw new Exception("No winner found.");
+        }
+
+        /* 
+         * Return weapons that the provided weapon beats.
+         * The are calculated in the following way:
+         * If the index of the weapon is even, then all even indices less than it,
+         * and all odd indices greater than it, are victories.
+         * One exception is if it is an odd index, and also the last index in the set,
+         * then the first index in the set is a victory.
+         */
+        public static IEnumerable<string> GetVictories(string[] weapons, string weapon)
+        {
+            // Gets index of weapon.
+            int index = Array.IndexOf(weapons, weapon);
+
+            // If weapon is odd and the final index in the set, then return the first item in the set as a victory.
+            if (index % 2 != 0 && index == weapons.Length - 1)
+                yield return weapons[0];
+
+            for (int i = index - 2; i >= 0; i -= 2)
+                yield return weapons[i];
+
+            for (int i = index + 1; i < weapons.Length; i += 2)
+                yield return weapons[i];
+        }
+
+        public string LastAIWeapon
+        {
+            private set;
+            get;
+        }
+
+        public readonly string[] Weapons;
+        private RPSAI _rpsAI;
+
+        class RPSAI
+        {
+            public RPSAI(params string[] weapons)
+            {
+                _weapons = weapons;
+                _weaponProbability = new Dictionary<string, int>();
+
+                // The AI sets the probability for each weapon to be chosen as 1.
+                foreach (string weapon in weapons)
+                    _weaponProbability.Add(weapon, 1);
+
+                _random = new Random();
+            }
+
+            // Increases probability of selecting each weapon that beats the provided move.
+            public void AddPlayerMove(string weapon)
+            {
+                int index = Array.IndexOf(_weapons, weapon);
+
+                foreach (string winWeapon in _weapons.Except(GetVictories(_weapons, weapon)))
+                    if (winWeapon != weapon)
+                        _weaponProbability[winWeapon]++;
+            }
+
+            // Gets the AI's next move.
+            public string NextMove()
+            {
+                double r = _random.NextDouble();
+
+                double divisor = _weaponProbability.Values.Sum();
+
+                var weightedWeaponRanges = new Dictionary<double, string>();
+
+                double currentPos = 0.0;
+
+                // Maps probabilities to ranges between 0.0 and 1.0. Returns weighted random weapon.
+                foreach (var weapon in _weaponProbability)
+                {
+                    double weightedRange = weapon.Value / divisor;
+                    if (r <= currentPos + (weapon.Value / divisor))
+                        return weapon.Key;
+                    currentPos += weightedRange;
+                }
+
+                throw new Exception("Error calculating move.");
+            }
+
+            Random _random;
+            private readonly string[] _weapons;
+            private Dictionary<string, int> _weaponProbability;
+        }
+    }
+}
